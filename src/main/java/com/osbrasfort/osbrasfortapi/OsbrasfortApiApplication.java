8cadaccbf34c131f43fddc6a0b475de47f4abcbc
Márcio Andrade | BRASFORT <marcio.andrade@brasfort.com.br>
2021-01-21 10:37:58 -0300

    iniciando projeto

---
+++ src/main/java/com/osbrasfort/osbrasfortapi/OsbrasfortApiApplication.java
@@ -0,0 +1,13 @@
+package com.osbrasfort.osbrasfortapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsbrasfortApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OsbrasfortApiApplication.class, args);
+    }
+
+}
